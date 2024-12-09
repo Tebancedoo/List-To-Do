db36a5f1ec6d035e605fda11b486c89eebd617c9
Tebancedoo <estebansalcedo211@gmail.com>
2024-12-09 16:50:58 -0500

    Add Front-End and Back-End

--- client/app/page.js
+++ client/app/page.js
@@ -1,95 +1,82 @@
-import Image from "next/image";
+"use client"
+
 import styles from "./page.module.css";
+import { useState } from "react";
+
+import FullCalendar from '@fullcalendar/react'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import interactionPlugin from '@fullcalendar/interaction'
+import timeGridPlugin from '@fullcalendar/timegrid'
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [selectedDate, setSelectedDate] = useState(null);
+  const [newTask, setNewTask] = useState("");  // Estado para almacenar la nueva tarea
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+  // Manejador del evento de selección de fecha
+  const handleDateClick = (arg) => {
+    setSelectedDate(arg.date);
+  };
+
+  // Función para agregar una tarea
+  const addTask = () => {
+    if (newTask) {
+
+      alert("Tarea agregada correctamente: " + newTask)
+      setNewTask(""); // Limpiar el campo de texto después de agregar la tarea
+    } else {
+      alert("No se ha ingresado una tarea");
+    }
+  };
+
+  // Función que se ejecuta cuando el valor del input cambia
+  const handleInputChange = (e) => {
+    setNewTask(e.target.value);
+  };
+
+  function handleClose() {
+    setSelectedDate(null)
+  };
+
+
+  return (
+    <main className={styles.main}>
+      <div className="grid grid-cols-10">
+        <div className="grid grid-cols-8">
+          <FullCalendar 
+            plugins={[dayGridPlugin, interactionPlugin, timeGridPlugin]} 
+            headerToolbar={{
+              left: 'prev,next today',
+              center: 'title',
+              right: 'dayGridMonth,timeGridWeek'
+            }}
+            events={{}}
+            nowIndicator={true}
+            editable={true}
+            droppable={true}
+            selectable={true}
+            selectMirror={true}
+            dateClick={handleDateClick} // Evento al hacer clic en una fecha
+          />
+
+          {/* Despliegue de tareas */}
+          {selectedDate && (
+              <div className="mt-2">
+                  <input
+                    className="custom-input"
+                    type="text"
+                    placeholder="Nueva tarea"
+                    value={newTask}  // Vincula el estado al valor del input
+                    onChange={handleInputChange} // Manejador de cambios en el input
+                  />
+                  <div className="button-container">
+                    <button className="submit-btn" onClick={addTask}>Agregar tarea</button> {/* Botón que agrega la tarea */}
+                    <button className="cancel-btn" onClick={handleClose}>Cancelar</button>
+                  </div>
+                </div>
+                
+          )}
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </div>
+    </main>
   );
 }
